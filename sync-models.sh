221900274b7a591e1f4966ab6a19bbb32d8907e9
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2023-02-20 00:31:57 -0600

    disk space fears

--- sync-models.sh
+++ sync-models.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo "THIS SCRIPT WILL DELETE ANY FILES NOT ON THE SOURCE"
-rsync --delete-after -rtyiP /mnt/MattNAS/Machine\ Learning/Sync/Models-VAEs/ ./data/StableDiffusion/
+rsync --delete-before -rtyiP /mnt/MattNAS/Machine\ Learning/Sync/Models-VAEs/ ./data/StableDiffusion/
 rsync --delete-after -rtyiP /mnt/MattNAS/Machine\ Learning/Sync/Hypernetworks/ ./data/Hypernetworks/
 rsync --delete-after -rtyiP /mnt/MattNAS/Machine\ Learning/Sync/Embeddings/ ./data/embeddings/
+rsync --delete-after -rtyiP /mnt/MattNAS/Machine\ Learning/Sync/LORA/ ./data/Lora/
