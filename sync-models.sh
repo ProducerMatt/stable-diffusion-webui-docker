b1c6995e1d4153629c4cf91bfdd09d91fc94ee45
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2023-03-27 00:41:44 -0500

    add controlnets

--- sync-models.sh
+++ sync-models.sh
@@ -5,3 +5,4 @@ rsync --delete-before -rtyiP /mnt/MattNAS/Machine\ Learning/Sync/Models-VAEs/ ./
 rsync --delete-after -rtyiP /mnt/MattNAS/Machine\ Learning/Sync/Hypernetworks/ ./data/Hypernetworks/
 rsync --delete-after -rtyiP /mnt/MattNAS/Machine\ Learning/Sync/Embeddings/ ./data/embeddings/
 rsync --delete-after -rtyiP /mnt/MattNAS/Machine\ Learning/Sync/LORA/ ./data/Lora/
+rsync --delete-after -rtyiP /mnt/MattNAS/Machine\ Learning/Sync/ControlNets/ ./data/config/auto/extensions/controlnet/models
