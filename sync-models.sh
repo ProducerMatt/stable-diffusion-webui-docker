03de14be745598a2826110de046277d79c1e4aa5
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2023-05-26 17:49:14 -0500

    make models sync last

--- sync-models.sh
+++ sync-models.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 echo "THIS SCRIPT WILL DELETE ANY FILES NOT ON THE SOURCE"
-rsync --delete-before -rtyiP /mnt/MattNAS/Machine\ Learning/Sync/Models-VAEs/ ./data/StableDiffusion/
 rsync --delete-after -rtyiP /mnt/MattNAS/Machine\ Learning/Sync/Hypernetworks/ ./data/Hypernetworks/
 rsync --delete-after -rtyiP /mnt/MattNAS/Machine\ Learning/Sync/Embeddings/ ./data/embeddings/
 rsync --delete-after -rtyiP /mnt/MattNAS/Machine\ Learning/Sync/LORA/ ./data/Lora/
 rsync --delete-after -rtyiP /mnt/MattNAS/Machine\ Learning/Sync/ControlNets/ ./data/config/auto/extensions/controlnet/models
+rsync --delete-before -rtyiP /mnt/MattNAS/Machine\ Learning/Sync/Models-VAEs/ ./data/StableDiffusion/
